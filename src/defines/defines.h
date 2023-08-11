e15668320e5e3ee0e13b7a84a3f7a65285c525d4
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-08-11 15:38:09 +0530

    basic color defines

---
+++ src/defines/defines.h
@@ -0,0 +1,19 @@
+/**
+ * @file defines.h
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
+#ifndef HORIZON_DEFINES_DEFINES_H
+#define HORIZON_DEFINES_DEFINES_H
+
+#define RED_FG "\033[1;91m"
+#define WHITE_FG "\033[1;97m"
+#define PURPLE_FG "\033[1;95m"
+#define YELLOW_FG "\033[1;93m"
+#define RESET_COLOR "\033[0m"
+
+#define ENCLOSE(clr, txt) \
+    clr txt RESET_COLOR
+
+#endif
