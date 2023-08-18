e55594dea3a25721ba77b5c5d1a8d1dee2c39255
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-08-18 10:50:34 +0530

    ADD: 1. contains `function_parameter_list` and `function_declaration` struct

---
+++ src/parser/function_declaration/function_declaration.hh
@@ -0,0 +1,32 @@
+/**
+ * @file function_declaration.hh
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
+#ifndef HORIZON_PARSER_FUNCTION_DECLARATION_HH
+#define HORIZON_PARSER_FUNCTION_DECLARATION_HH
+
+#include "../../../deps/string/string.hh"
+#include <map>
+
+namespace horizon
+{
+    namespace horizon_parser
+    {
+        struct function_parameter_list
+        {
+            //       1. Identifier         2. Type
+            std::map<horizon_deps::string, horizon_deps::vector<horizon_deps::string>> M_container;
+        };
+
+        struct function_declaration
+        {
+            horizon_deps::string M_identifier;
+            function_parameter_list M_parameter;
+            horizon_deps::vector<horizon_deps::string> M_return_type;
+        };
+    }
+}
+
+#endif
