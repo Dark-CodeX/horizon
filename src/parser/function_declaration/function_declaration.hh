993fed9c4bd4d5d670addb5025939e0c86a4a702
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-08-24 15:58:35 +0530

    UPDATE: [IGNORE COMMIT] 1. in progress

--- src/parser/function_declaration/function_declaration.hh
+++ src/parser/function_declaration/function_declaration.hh
@@ -7,8 +7,9 @@
 #ifndef HORIZON_PARSER_FUNCTION_DECLARATION_HH
 #define HORIZON_PARSER_FUNCTION_DECLARATION_HH
 
-#include "../../../deps/string/string.hh"
 #include <map>
+
+#include "../../../deps/string/string.hh"
 
 namespace horizon
 {
@@ -20,10 +21,16 @@ namespace horizon
             std::map<horizon_deps::string, horizon_deps::vector<horizon_deps::string>> M_container;
         };
 
+        struct function_template_list
+        {
+        };
+
         struct function_declaration
         {
             horizon_deps::string M_identifier;
             function_parameter_list M_parameter;
+            bool M_is_template;
+            function_template_list M_template;
             horizon_deps::vector<horizon_deps::string> M_return_type;
         };
     }
