e79f64a7ca0a2c62957967ab36283736f2c9f043
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-08-24 15:47:21 +0530

    UPDATE: 1. broken `misc.hh` into 2 separate files

--- src/misc/exit_heap_fail.hh
+++ src/misc/exit_heap_fail.hh
@@ -1,30 +1,22 @@
 /**
- * @file misc.hh
+ * @file exit_heap_fail.hh
  * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
  * @author Tushar Chaurasia (Dark-CodeX)
  */
 
-#ifndef HORIZON_MISC_MISC_HH
-#define HORIZON_MISC_MISC_HH
+#ifndef HORIZON_MISC_EXIT_HEAP_FAIL_HH
+#define HORIZON_MISC_EXIT_HEAP_FAIL_HH
 
 #include <cstdio>
 #include <cerrno>
 
-#include "./file/file.hh"
-#include "../../deps/string/string.hh"
 #include "../colorize/colorize.h"
-#include "../defines/defines.h"
 
 namespace horizon
 {
     namespace horizon_misc
     {
-        class misc
-        {
-        public:
-            [[nodiscard]] static HR_FILE *load_file(const char *loc);
-            static void exit_heap_fail(const void *ptr, const char *__s);
-        };
+        void exit_heap_fail(const void *ptr, const char *__s);
     }
 }
 
---
+++ src/misc/load_file.hh
@@ -0,0 +1,27 @@
+/**
+ * @file load_file.hh
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
+#ifndef HORIZON_MISC_MISC_HH
+#define HORIZON_MISC_MISC_HH
+
+#include <cstdio>
+#include <cerrno>
+
+#include "./file/file.hh"
+#include "../../deps/sptr/sptr.hh"
+#include "../../deps/string/string.hh"
+#include "../colorize/colorize.h"
+#include "../defines/defines.h"
+
+namespace horizon
+{
+    namespace horizon_misc
+    {
+        [[nodiscard]] horizon_deps::sptr<HR_FILE> load_file(const char *loc);
+    }
+}
+
+#endif
