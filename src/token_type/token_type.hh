6285b184bc8470ae7a2faa35e4a61fdbe5be46fe
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-10-23 08:15:58 +0530

    UPDATE: 1. added helpful comments for each enum

--- src/token_type/token_type.hh
+++ src/token_type/token_type.hh
@@ -11,74 +11,59 @@ namespace horizon
 {
     enum class token_type : unsigned char // max 256 tokens
     {
-        TOKEN_IDENTIFIER,
-
-        TOKEN_CHAR_LITERAL,
-        TOKEN_STRING_LITERAL,
-
-        TOKEN_INTEGER_LITERAL,
-        TOKEN_DECIMAL_LITERAL,
-
-        TOKEN_ARITHMETIC_ADD,
-        TOKEN_ARITHMETIC_SUBSTRACT,
-        TOKEN_ARITHMETIC_MULTIPLY,
-        TOKEN_ARITHMETIC_POWER,
-        TOKEN_ARITHMETIC_DIVIDE,
-        TOKEN_ARITHMETIC_MODULUS,
-
-        TOKEN_RELATIONAL_EQUAL_TO,
-        TOKEN_RELATIONAL_NOT_EQUAL_TO,
-        TOKEN_RELATIONAL_GREATER_THAN,
-        TOKEN_RELATIONAL_LESS_THAN,
-        TOKEN_RELATIONAL_GREATER_THAN_OR_EQUAL_TO,
-        TOKEN_RELATIONAL_LESS_THAN_OR_EQUAL_TO,
-
-        TOKEN_LOGICAL_NOT,
-        TOKEN_LOGICAL_AND,
-        TOKEN_LOGICAL_OR,
-
-        TOKEN_BITWISE_NOT,
-        TOKEN_BITWISE_AND,
-        TOKEN_BITWISE_OR,
-        TOKEN_BITWISE_XOR,
-        TOKEN_BITWISE_LEFT_SHIFT,
-        TOKEN_BITWISE_RIGHT_SHIFT,
-
-        TOKEN_ASSIGN,
-        TOKEN_ASSIGN_ADD,
-        TOKEN_ASSIGN_SUBSTRACT,
-        TOKEN_ASSIGN_MULTIPLY,
-        TOKEN_ASSIGN_DIVIDE,
-        TOKEN_ASSIGN_MODULUS,
-        TOKEN_ASSIGN_BITWISE_AND,
-        TOKEN_ASSIGN_BITWISE_OR,
-        TOKEN_ASSIGN_BITWISE_XOR,
-        TOKEN_ASSIGN_LEFT_SHIFT,
-        TOKEN_ASSIGN_RIGHT_SHIFT,
-
-        TOKEN_INCREMENT,
-        TOKEN_DECREMENT,
-
-        TOKEN_QUESTION,
-
-        TOKEN_SEMICOLON,
-        TOKEN_COLON,
-        TOKEN_COMMA,
-        TOKEN_DOT,
-
-        TOKEN_RIGHT_PAREN,
-        TOKEN_LEFT_PAREN,
-
-        TOKEN_RIGHT_BRACE,
-        TOKEN_LEFT_BRACE,
-
-        TOKEN_RIGHT_BRACKET,
-        TOKEN_LEFT_BRACKET,
-
-        TOKEN_KEYWORD,
-        TOKEN_PRIMARY_TYPE,
-
-        TOKEN_END_OF_FILE
+        TOKEN_IDENTIFIER,                          // Represents any identifier
+        TOKEN_CHAR_LITERAL,                        // Represents any single-byte character
+        TOKEN_STRING_LITERAL,                      // Represents any string
+        TOKEN_INTEGER_LITERAL,                     // Represents any integer in any format
+        TOKEN_DECIMAL_LITERAL,                     // Represents any floating point integer in any format
+        TOKEN_ARITHMETIC_ADD,                      // Represents addition operator (+)
+        TOKEN_ARITHMETIC_SUBSTRACT,                // Represents subtraction operator (-)
+        TOKEN_ARITHMETIC_MULTIPLY,                 // Represents multiplication operator (*)
+        TOKEN_ARITHMETIC_POWER,                    // Represents exponentiation operator (^)
+        TOKEN_ARITHMETIC_DIVIDE,                   // Represents division operator (/)
+        TOKEN_ARITHMETIC_MODULUS,                  // Represents modulus operator (%)
+        TOKEN_RELATIONAL_EQUAL_TO,                 // Represents equal-to operator (==)
+        TOKEN_RELATIONAL_NOT_EQUAL_TO,             // Represents not-equal-to operator (!=)
+        TOKEN_RELATIONAL_GREATER_THAN,             // Represents greater-than operator (>)
+        TOKEN_RELATIONAL_LESS_THAN,                // Represents less-than operator (<)
+        TOKEN_RELATIONAL_GREATER_THAN_OR_EQUAL_TO, // Represents greater-than-or-equal-to operator (>=)
+        TOKEN_RELATIONAL_LESS_THAN_OR_EQUAL_TO,    // Represents less-than-or-equal-to operator (<=)
+        TOKEN_LOGICAL_NOT,                         // Represents logical NOT operator (!)
+        TOKEN_LOGICAL_AND,                         // Represents logical AND operator (&&)
+        TOKEN_LOGICAL_OR,                          // Represents logical OR operator (||)
+        TOKEN_BITWISE_NOT,                         // Represents bitwise NOT operator (~)
+        TOKEN_BITWISE_AND,                         // Represents bitwise AND operator (&)
+        TOKEN_BITWISE_OR,                          // Represents bitwise OR operator (|)
+        TOKEN_BITWISE_XOR,                         // Represents bitwise XOR operator (^)
+        TOKEN_BITWISE_LEFT_SHIFT,                  // Represents left shift operator (<<)
+        TOKEN_BITWISE_RIGHT_SHIFT,                 // Represents right shift operator (>>)
+        TOKEN_ASSIGN,                              // Represents simple assignment operator (=)
+        TOKEN_ASSIGN_ADD,                          // Represents addition assignment operator (+=)
+        TOKEN_ASSIGN_SUBSTRACT,                    // Represents subtraction assignment operator (-=)
+        TOKEN_ASSIGN_MULTIPLY,                     // Represents multiplication assignment operator (*=)
+        TOKEN_ASSIGN_DIVIDE,                       // Represents division assignment operator (/=)
+        TOKEN_ASSIGN_MODULUS,                      // Represents modulus assignment operator (%=)
+        TOKEN_ASSIGN_BITWISE_AND,                  // Represents bitwise AND assignment operator (&=)
+        TOKEN_ASSIGN_BITWISE_OR,                   // Represents bitwise OR assignment operator (|=)
+        TOKEN_ASSIGN_BITWISE_XOR,                  // Represents bitwise XOR assignment operator (^=)
+        TOKEN_ASSIGN_LEFT_SHIFT,                   // Represents left shift assignment operator (<<=)
+        TOKEN_ASSIGN_RIGHT_SHIFT,                  // Represents right shift assignment operator (>>=)
+        TOKEN_INCREMENT,                           // Represents increment operator (++)
+        TOKEN_DECREMENT,                           // Represents decrement operator (--)
+        TOKEN_QUESTION,                            // Represents the ternary conditional operator (?)
+        TOKEN_SEMICOLON,                           // Represents semicolon (;)
+        TOKEN_COLON,                               // Represents colon (:)
+        TOKEN_COMMA,                               // Represents comma (,)
+        TOKEN_DOT,                                 // Represents dot (.)
+        TOKEN_RIGHT_PAREN,                         // Represents right parenthesis )
+        TOKEN_LEFT_PAREN,                          // Represents left parenthesis (
+        TOKEN_RIGHT_BRACE,                         // Represents right brace }
+        TOKEN_LEFT_BRACE,                          // Represents left brace {
+        TOKEN_RIGHT_BRACKET,                       // Represents right bracket ]
+        TOKEN_LEFT_BRACKET,                        // Represents left bracket [
+        TOKEN_KEYWORD,                             // Represents pre-defined keywords
+        TOKEN_PRIMARY_TYPE,                        // Represents primary data types
+        TOKEN_END_OF_FILE                          // Represents EOF of the opened file
     };
 }
 
