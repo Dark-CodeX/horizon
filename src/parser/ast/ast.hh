9d4915169d8244016bb188decfa570b71abe4b51
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2025-01-09 18:34:08 +0530

    ADD: 1. basic structure of `ast_node` and its derived classes

---
+++ src/parser/ast/ast.hh
@@ -0,0 +1,122 @@
+/**
+ * @file ast.hh
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
+#ifndef HORIZON_PARSER_AST_AST_HH
+#define HORIZON_PARSER_AST_AST_HH
+
+#include <iostream>
+
+#include "../../../deps/sptr/sptr.hh"
+#include "../../token_type/token_type.hh"
+#include "../../../deps/string/string.hh"
+
+namespace horizon
+{
+    namespace horizon_parser
+    {
+        class ast_node
+        {
+          public:
+            virtual ~ast_node() = default;
+            virtual void print() const = 0;
+        };
+
+        template <typename T>
+        class ast_operand_node : public ast_node
+        {
+            T M_val;
+
+          public:
+            inline ast_operand_node(const T &val)
+                : M_val(val) {}
+            inline void print() const override
+            {
+                if constexpr (std::is_same<T, horizon_deps::string>::value)
+                    std::cout << this->M_val.c_str();
+                else
+                    std::cout << this->M_val;
+            }
+        };
+
+        class ast_binary_operation_node : public ast_node
+        {
+            horizon_deps::sptr<ast_node> M_left;
+            token_type M_operator;
+            horizon_deps::sptr<ast_node> M_right;
+
+          public:
+            inline ast_binary_operation_node(horizon_deps::sptr<ast_node> &&left, token_type opr, horizon_deps::sptr<ast_node> &&right)
+                : M_left(std::move(left)), M_operator(opr), M_right(std::move(right)) {}
+
+            inline void print() const override
+            {
+                const char *to_str[] =
+                    {
+                        "TOKEN_IDENTIFIER",
+                        "TOKEN_CHAR_LITERAL",
+                        "TOKEN_STRING_LITERAL",
+                        "TOKEN_INTEGER_LITERAL",
+                        "TOKEN_DECIMAL_LITERAL",
+                        "+",
+                        "-",
+                        "*",
+                        "**",
+                        "/",
+                        "%",
+                        "==",
+                        "!=",
+                        ">",
+                        "<",
+                        ">=",
+                        "<=",
+                        "!",
+                        "&&",
+                        "||",
+                        "~",
+                        "&",
+                        "|",
+                        "^",
+                        "<<",
+                        ">>",
+                        "=",
+                        "+=",
+                        "-=",
+                        "*=",
+                        "**=",
+                        "/=",
+                        "%=",
+                        "&=",
+                        "|=",
+                        "^=",
+                        "<<=",
+                        ">>=",
+                        "++",
+                        "--",
+                        "?",
+                        ";",
+                        ":",
+                        ",",
+                        ".",
+                        ")",
+                        "(",
+                        "}",
+                        "{",
+                        "]",
+                        "[",
+                        "TOKEN_KEYWORD",
+                        "TOKEN_PRIMARY_TYPE",
+                        "TOKEN_END_OF_FILE"};
+                printf("( ");
+                this->M_left->print();
+                std::cout << " " << to_str[(unsigned)this->M_operator] << " ";
+                this->M_right->print();
+                printf(" )");
+            }
+        };
+    }
+}
+
+#endif
